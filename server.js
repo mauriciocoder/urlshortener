d568d96de7be6513cf948f8360b3ee7cafa100a1
Mauricio Bonetti <bonetti.mauricio@gmail.com>
2016-08-08 21:51:34 +0000

    Package update

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ var app = express();
 app.use(express.static("public"));
 app.get("/short/*", function(req, resp) {
     var url = req.params[0];
-    var shortUrl = service.short(url);
-    resp.json(shortUrl);
+    service.short(url, function(sequence) {
+       console.log("chegou aki - sequence = " + sequence);
+       resp.json(sequence); 
+    });
 });
 var port = process.env.PORT || 8080;
-app.listen(port);
+app.listen(port, function() {
+    console.log("URLshortener app listening on port " + process.env.PORT);
+});
